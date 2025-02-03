e85bb02a813673d81c66e856296c66a3d0b0fd21
Francesco Poli <polifr@gmail.com>
2025-02-04 00:25:56 +0100

    Gestione configurazione

--- ms-rust-axum/src/main.rs
+++ ms-rust-axum/src/main.rs
@@ -1,4 +1,12 @@
+use std::net::SocketAddr;
+use tracing::{debug, info};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use crate::config::Config;
+
 use axum::{Router, routing::get};
+
+mod config;
 
 async fn hello() -> &'static str {
     "Hello, Rust! This is a microservice!"
@@ -6,7 +14,29 @@ async fn hello() -> &'static str {
 
 #[tokio::main]
 async fn main() {
+    info!("Starting...");
+
+    debug!("Dotenv initializing...");
+    let var_name = dotenv::from_filename(
+        std::env::var("ENV_FILENAME").unwrap_or(".env.release".to_string())
+    ).ok();
+    debug!("Dotenv initialized: {}", &var_name.unwrap().display());
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let config = Config::init();
+
+    info!("Connecting to pg: {}", &config.database_url);
+    info!("Connecting to cache: {}", &config.cache_url);
+
     let app = Router::new().route("/", get(hello));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    info!("Application started.");
 }
