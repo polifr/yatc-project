5bce70e04823fa6a5b457bae7fa474d0a4edbf0d
Francesco Poli <polifr@gmail.com>
2025-02-22 02:58:09 +0100

    Bozza per pagina centrale gw

---
+++ gw-spring-cloud/src/main/java/it/poli/controller/IndexController.java
@@ -0,0 +1,15 @@
+package it.poli.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class IndexController {
+
+  @GetMapping(path = {"/", "/index.htm", "/index.html"})
+  public Mono<Rendering> loginTitolare() {
+    return Mono.just(Rendering.view("/index").build());
+  }
+}
