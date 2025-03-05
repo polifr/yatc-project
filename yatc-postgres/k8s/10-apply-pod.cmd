fbc6dfe18b3c5a0330cc539b18e852cca81044a9
Francesco Poli <francesco.poli@alten.it>
2025-03-05 15:42:13 +0100

    Configurazione exporter per pg su k8s

--- yatc-postgres/k8s/10-apply-pod.cmd
+++ yatc-postgres/k8s/10-apply-pod.cmd
@@ -1,2 +1,5 @@
 kubectl apply -f yatc-postgres-deployment.yaml
 kubectl apply -f yatc-postgres-service.yaml
+
+kubectl apply -f yatc-postgres-exporter-deployment.yaml
+kubectl apply -f yatc-postgres-exporter-service.yaml
--- yatc-postgres/k8s/99-delete-all.cmd
+++ yatc-postgres/k8s/99-delete-all.cmd
@@ -1,3 +1,5 @@
+kubectl delete service yatc-postgres-exporter
+kubectl delete deployment yatc-postgres-exporter
 kubectl delete service yatc-postgres
 kubectl delete deployment yatc-postgres
 kubectl delete secret yatc-postgres-secret
