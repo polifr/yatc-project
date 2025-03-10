88466ea9899d5d82ad57cc88b99dcf7972a16239
Francesco Poli <polifr@gmail.com>
2025-03-10 09:35:18 +0100

    Bozza progetti ms spring tramite initializr

---
+++ ms-spring-servlet/src/main/java/it/poli/MsSpringServletApplication.java
@@ -0,0 +1,13 @@
+package it.poli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringServletApplication.class, args);
+	}
+
+}
---
+++ ms-spring-servlet/src/test/java/it/poli/MsSpringServletApplicationTests.java
@@ -0,0 +1,13 @@
+package it.poli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
