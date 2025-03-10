082d1c4f533939d66d61045461b638744612b0bc
Francesco Poli <polifr@gmail.com>
2025-03-10 18:57:26 +0100

    Refactor configurazione spring security

--- ms-spring-reactive/src/main/java/it/poli/security/SecurityConfig.java
+++ ms-spring-reactive/src/main/java/it/poli/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.info.InfoEndpoint;
-import org.springframework.boot.actuate.logging.LoggersEndpoint;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.context.annotation.Bean;
@@ -25,32 +24,27 @@ public class SecurityConfig {
   @Bean
   SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
 
-    http.formLogin(FormLoginSpec::disable);
-    http.httpBasic(HttpBasicSpec::disable);
-    http.logout(LogoutSpec::disable);
-    http.csrf(CsrfSpec::disable);
-
-    http.authorizeExchange(
-        authorize ->
-            authorize
-                .matchers(
-                    EndpointRequest.to(
-                        HealthEndpoint.class,
-                        InfoEndpoint.class,
-                        LoggersEndpoint.class,
-                        MetricsEndpoint.class,
-                        PrometheusScrapeEndpoint.class))
-                .permitAll());
-
-    http.authorizeExchange(
-        authorize ->
-            authorize
-                .pathMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**")
-                .permitAll());
-
-    http.authorizeExchange(authorize -> authorize.anyExchange().authenticated())
-        .oauth2ResourceServer(rs -> rs.jwt(withDefaults()));
-
-    return http.build();
+    return http.formLogin(FormLoginSpec::disable)
+        .httpBasic(HttpBasicSpec::disable)
+        .logout(LogoutSpec::disable)
+        .csrf(CsrfSpec::disable)
+        .authorizeExchange(
+            authorize ->
+                authorize
+                    .matchers(
+                        EndpointRequest.to(
+                            HealthEndpoint.class,
+                            InfoEndpoint.class,
+                            MetricsEndpoint.class,
+                            PrometheusScrapeEndpoint.class))
+                    .permitAll())
+        .authorizeExchange(
+            authorize ->
+                authorize
+                    .pathMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**")
+                    .permitAll())
+        .authorizeExchange(authorize -> authorize.anyExchange().authenticated())
+        .oauth2ResourceServer(rs -> rs.jwt(withDefaults()))
+        .build();
   }
 }
