25267bf26c3d1519b13b63e7223a3c87bf680bd3
Francesco Poli <polifr@gmail.com>
2025-03-14 21:27:27 +0100

    Fix su build maven per linux k8s

--- gw-spring-cloud/k8s/09-build-image.sh
+++ gw-spring-cloud/k8s/09-build-image.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# docker run -it --rm -u `id -u`:`id -g` -v "$PWD/..":/usr/src/mymaven -v ~/.m2:/var/maven/.m2 -w /usr/src/mymaven maven:3-eclipse-temurin-21 mvn -Duser.home=/var/maven -Dmaven.test.skip=true clean package
-
-docker run -it --rm -v "$(pwd)/..":/opt/maven -v "$(home)/.m2":/opt/maven-repo/.m2 -w /opt/maven maven:3-eclipse-temurin-21 mvn -Duser.home=/opt/maven-repo -Dmaven.test.skip=true clean package
+docker run -it --rm -v $PWD/..:/opt/maven -v $HOME/.m2:/opt/maven-repo/.m2 -w /opt/maven maven:3-eclipse-temurin-21 mvn -Duser.home=/opt/maven-repo -Dmaven.test.skip=true clean package
 
 eval $(minikube -p minikube docker-env)
 
-export MODULE_NAME="gw-spring-cloud"
+export MODULE_NAME="yatc/gw-spring-cloud"
 docker build --tag ${MODULE_NAME} --label ${MODULE_NAME} -f ../Dockerfile.k8s ../. 
 docker image prune --force --filter='label='"${MODULE_NAME}"''
