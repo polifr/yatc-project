4abc2018b55f212c720403899361c70006ec0376
Francesco Poli <polifr@gmail.com>
2025-03-15 22:59:26 +0100

    Attivazione monitoraggio prometheus su nginx

--- fe-angular-material/k8s/10-apply-pod.sh
+++ fe-angular-material/k8s/10-apply-pod.sh
@@ -2,3 +2,5 @@
 
 kubectl apply -f fe-angular-material-deployment.yaml
 kubectl apply -f fe-angular-material-service.yaml
+kubectl apply -f fe-angular-material-exporter-deployment.yaml
+kubectl apply -f fe-angular-material-exporter-service.yaml
--- fe-angular-material/k8s/99-delete-all.sh
+++ fe-angular-material/k8s/99-delete-all.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+kubectl delete service fe-angular-material-exporter
+kubectl delete deployment fe-angular-material-exporter
 kubectl delete service fe-angular-material
 kubectl delete deployment fe-angular-material
