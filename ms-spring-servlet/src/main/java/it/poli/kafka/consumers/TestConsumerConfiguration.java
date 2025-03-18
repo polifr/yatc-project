3e031fff87ac91af30498e501fe9ed231e85e275
Francesco Poli <polifr@gmail.com>
2025-03-18 01:03:28 +0100

    Configurazione consumer di prova

---
+++ ms-spring-servlet/src/main/java/it/poli/kafka/consumers/TestConsumerConfiguration.java
@@ -0,0 +1,17 @@
+package it.poli.kafka.consumers;
+
+import it.poli.kafka.events.TestEvent;
+import java.util.function.Consumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class TestConsumerConfiguration {
+
+  @Bean
+  Consumer<TestEvent> testEventConsumer() {
+    return c -> log.info("Message received: {}", c.getMessage());
+  }
+}
---
+++ ms-spring-servlet/src/main/java/it/poli/kafka/events/TestEvent.java
@@ -0,0 +1,17 @@
+package it.poli.kafka.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TestEvent {
+
+  private String message;
+}
