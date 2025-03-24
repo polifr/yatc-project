4f894727acc21561b967040d9eedfee7c3c893ab
Francesco Poli <polifr@gmail.com>
2025-03-24 01:08:01 +0100

    Annotazioni costruttore

--- ms-spring-servlet/src/main/java/it/poli/kafka/consumers/TestConsumerConfiguration.java
+++ ms-spring-servlet/src/main/java/it/poli/kafka/consumers/TestConsumerConfiguration.java
@@ -2,12 +2,14 @@ package it.poli.kafka.consumers;
 
 import it.poli.kafka.events.TestEvent;
 import java.util.function.Consumer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class TestConsumerConfiguration {
 
   @Bean
--- ms-spring-servlet/src/main/java/it/poli/kafka/producers/TestProducerConfiguration.java
+++ ms-spring-servlet/src/main/java/it/poli/kafka/producers/TestProducerConfiguration.java
@@ -3,11 +3,13 @@ package it.poli.kafka.producers;
 import it.poli.kafka.events.TestEvent;
 import java.util.UUID;
 import java.util.function.Supplier;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 @Slf4j
 public class TestProducerConfiguration {
 
