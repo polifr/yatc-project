6e57ef6a8080c71eecaffa3980728ceeaed5a488
Francesco Poli <polifr@gmail.com>
2025-04-09 16:24:52 +0200

    Invio messaggi kafka tramite post

---
+++ ms-spring-servlet/src/main/java/it/poli/controller/MessageController.java
@@ -0,0 +1,30 @@
+package it.poli.controller;
+
+import it.poli.controller.model.MessageModel;
+import it.poli.kafka.events.TestEvent;
+import it.poli.kafka.producers.OnPostEventProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/spring-servlet/message")
+@RequiredArgsConstructor
+@Slf4j
+public class MessageController {
+
+  private final OnPostEventProducer onPostEventProducer;
+
+  @PostMapping("/v1")
+  public ResponseEntity<Void> sendMessage(@RequestBody MessageModel message) {
+    log.debug("Ricevuta richiesta di invio del messaggio >{}<", message);
+    boolean ret =
+        onPostEventProducer.publishTestEvent(
+            TestEvent.builder().message(message.getMessageBody()).build());
+    return ret ? ResponseEntity.ok().build() : ResponseEntity.internalServerError().build();
+  }
+}
--- ms-spring-servlet/src/main/java/it/poli/controller/TestController.java
+++ ms-spring-servlet/src/main/java/it/poli/controller/TestController.java
@@ -1,16 +1,21 @@
 package it.poli.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/spring-servlet")
+@RequestMapping("/api/spring-servlet/test")
+@RequiredArgsConstructor
+@Slf4j
 public class TestController {
 
-  @GetMapping("/test/v1")
+  @GetMapping("/v1")
   public ResponseEntity<String> testV1() {
+    log.debug("Ricevuta richiesta su endpoint di test.");
     return ResponseEntity.ok("This is a Spring Servlet microservice!");
   }
 }
