4d87529b57b3a79fcca4343b5189b421421606d5
Francesco Poli <francesco.poli@alten.it>
2025-04-13 00:36:35 +0200

    Script e configurazioni kafka

--- yatc-kafka/k8s/00-init-k8s.sh
+++ yatc-kafka/k8s/00-init-k8s.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 ./99-delete-all.sh
+sleep 5
 ./01-create-configmap.sh
+sleep 5
 ./10-apply-pod.sh
--- yatc-kafka/k8s/10-apply-pod.sh
+++ yatc-kafka/k8s/10-apply-pod.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-kubectl apply -f yatc-kafka-deployment.yaml
 kubectl apply -f yatc-kafka-service.yaml
-kubectl apply -f yatc-kafka-exporter-deployment.yaml
+sleep 3
+kubectl apply -f yatc-kafka-deployment.yaml
+
 kubectl apply -f yatc-kafka-exporter-service.yaml
+sleep 3
+kubectl apply -f yatc-kafka-exporter-deployment.yaml
