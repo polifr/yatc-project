da236ddff9b7b6c6a9ab362434d409411028e3d7
Francesco Poli <polifr@gmail.com>
2025-04-14 00:16:44 +0200

    Attivazione form

--- fe-angular-material/src/app/app.component.ts
+++ fe-angular-material/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormPageComponent } from './form-page/form-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormPageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ fe-angular-material/src/app/form-page/form-page.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-form-page',
+  imports: [CommonModule, ReactiveFormsModule, MatCardModule, MatButtonModule, MatInputModule],
+  templateUrl: './form-page.component.html',
+  styleUrl: './form-page.component.scss'
+})
+export class FormPageComponent {
+  form: FormGroup;
+  responseData: any;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      textInput: ['']
+    });
+  }
+
+  submitForm() {
+    const message = this.form.value.textInput;
+    const requestBody = { messageBody: message };
+
+    console.log('SUbmit del messaggio ' + message);
+
+    this.http.post<any>('/api/spring-servlet/message/v1', requestBody)
+      .subscribe({
+        next: data => this.responseData = data,
+        error: err => console.error('Errore nella richiesta:', err)
+      });
+  }
+}
