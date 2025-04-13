2a6401c005afb7598937d82dc5ed0671c348b835
Francesco Poli <polifr@gmail.com>
2025-04-14 01:06:15 +0200

    Annotazioni Transactional per messaggi kafka

--- ms-spring-servlet/src/main/java/it/poli/controller/MessageController.java
+++ ms-spring-servlet/src/main/java/it/poli/controller/MessageController.java
@@ -6,6 +6,7 @@ import it.poli.kafka.producers.OnPostEventProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class MessageController {
   private final OnPostEventProducer onPostEventProducer;
 
   @PostMapping("/v1")
+  @Transactional
   public ResponseEntity<Void> sendMessage(@RequestBody MessageModel message) {
     log.debug("Ricevuta richiesta di invio del messaggio >{}<", message);
     boolean ret =
--- ms-spring-servlet/src/main/java/it/poli/kafka/producers/OnPostEventProducer.java
+++ ms-spring-servlet/src/main/java/it/poli/kafka/producers/OnPostEventProducer.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -13,6 +14,7 @@ public class OnPostEventProducer {
 
   private final StreamBridge streamBridge;
 
+  @Transactional
   public boolean publishTestEvent(TestEvent testEvent) {
     return streamBridge.send("yatc-test-topic", testEvent);
   }
