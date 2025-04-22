0013061fc5055f038cf9b5775d28be49e54d7ac3
Francesco Poli <polifr@gmail.com>
2025-04-22 20:29:17 +0200

    Avanzamento kafka su rust

--- ms-rust-axum/src/main.rs
+++ ms-rust-axum/src/main.rs
@@ -1,4 +1,5 @@
-use tracing::{debug, info};
+use rdkafka::{config::RDKafkaLogLevel, consumer::{Consumer, CommitMode, StreamConsumer}, util::get_rdkafka_version, ClientConfig, Message, message::Headers};
+use tracing::{debug, info, warn};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use crate::config::Config;
@@ -9,6 +10,51 @@ mod config;
 
 async fn hello() -> &'static str {
     "Hello, Rust! This is a microservice!"
+}
+
+fn create_consumer(bootstrap_servers: &str, group_id: &str) -> StreamConsumer {
+    ClientConfig::new()
+        .set("bootstrap.servers", bootstrap_servers)
+        .set("group.id", group_id)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create()
+        .expect("Errore in fase di creazione del client Kafka.")
+}
+
+async fn consume_and_print(bootstrap_servers: &str, group_id: &str, topics: &[&str]) {
+
+    let consumer = create_consumer(bootstrap_servers, group_id);
+
+    consumer
+        .subscribe(&topics.to_vec())
+        .expect("Errore in fase di sottoscrizione ai topic.");
+
+    loop {
+        match consumer.recv().await {
+            Err(e) => warn!("Kafka error: {}", e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        warn!("Error while deserializing message payload: {:?}", e);
+                        ""
+                    }
+                };
+                info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                if let Some(headers) = m.headers() {
+                    for header in headers.iter() {
+                        info!("  Header {:#?}: {:?}", header.key, header.value);
+                    }
+                }
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        };
+    }
 }
 
 #[tokio::main]
@@ -37,6 +83,15 @@ async fn main() {
     let app = Router::new().route("/api/rust-axum/test/v1", get(hello));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    let (version_n, version_s) = get_rdkafka_version();
+    info!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
+    
+    consume_and_print(
+        "yatc-kafka:9092",
+        "ms-rust-axum", 
+        &["yatc-test-topic"]
+    ).await;
 
     info!("Application started.");
 }
