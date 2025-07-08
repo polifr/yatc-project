f3efed68f1f92e261a61d7bcd104c92af4b9e62a
polifr <polifr@gmail.com>
2025-07-08 16:05:35 +0200

    Junit test di caricamento del contesto di base

---
+++ gw-spring-cloud/src/test/java/it/poli/GwSpringCloudApplicationTest.java
@@ -0,0 +1,27 @@
+package it.poli;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest
+class GwSpringCloudApplicationTest {
+
+	  @MockitoBean private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+	  @MockitoBean private ReactiveJwtDecoder jwtDecoder;
+
+	  @Autowired(required = false)
+	  private ApplicationContext applicationContext;
+
+	  @Test
+	  void contextLoads() {
+	    assertNotNull(applicationContext, "applicationContext null");
+	  }
+}
