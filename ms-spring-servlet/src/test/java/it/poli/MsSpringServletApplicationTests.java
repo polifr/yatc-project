e9915b23c06e8775498b41f7f8a0b7f1cfdacdd7
polifr <polifr@gmail.com>
2025-07-08 16:05:17 +0200

    Verifica della creazione del contesto

--- ms-spring-servlet/src/test/java/it/poli/MsSpringServletApplicationTests.java
+++ ms-spring-servlet/src/test/java/it/poli/MsSpringServletApplicationTests.java
@@ -1,11 +1,20 @@
 package it.poli;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class MsSpringServletApplicationTests {
 
+  @Autowired(required = false)
+  private ApplicationContext applicationContext;
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertNotNull(applicationContext, "applicationContext null");
+  }
 }
