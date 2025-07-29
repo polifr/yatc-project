2cc8f609dd1d82b3cf943029a9a706f1636c25a5
polifr <polifr@gmail.com>
2025-07-29 22:25:38 +0200

    Fix -> ServerBearerExchangeFilterFunction

--- ms-spring-reactive/src/main/java/it/poli/client/filter/FilterFunctionConfiguration.java
+++ ms-spring-reactive/src/main/java/it/poli/client/filter/FilterFunctionConfiguration.java
@@ -4,21 +4,16 @@ import java.util.List;
 import java.util.function.Consumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.server.resource.web.reactive.function.client.ServletBearerExchangeFilterFunction;
+import org.springframework.security.oauth2.server.resource.web.reactive.function.client.ServerBearerExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 
 @Configuration(proxyBeanMethods = false)
 public class FilterFunctionConfiguration {
 
   @Bean
-  ServletBearerExchangeFilterFunction servletBearerExchangeFilterFunction() {
-    return new ServletBearerExchangeFilterFunction();
-  }
-
-  @Bean
   Consumer<List<ExchangeFilterFunction>> exchangeFilterFunctionList() {
     return (List<ExchangeFilterFunction> filters) -> {
-      filters.add(new ServletBearerExchangeFilterFunction());
+      filters.add(new ServerBearerExchangeFilterFunction());
     };
   }
 }
