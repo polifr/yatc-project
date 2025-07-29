b446db177846b298fab640b213aeaf8f557005e6
polifr <polifr@gmail.com>
2025-07-29 17:13:38 +0200

    Rimozione client verso gw

--- ms-spring-servlet/src/main/java/it/poli/controller/BridgeController.java
+++ ms-spring-servlet/src/main/java/it/poli/controller/BridgeController.java
@@ -1,6 +1,5 @@
 package it.poli.controller;
 
-import it.poli.client.GwSpringCloudClient;
 import it.poli.client.MsRustAxumClient;
 import it.poli.client.MsSpringReactiveClient;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +17,6 @@ public class BridgeController {
 
   private final MsSpringReactiveClient msSpringReactiveClient;
   private final MsRustAxumClient msRustAxumClient;
-  private final GwSpringCloudClient gwSpringCloudClient;
 
   @GetMapping("/spring-reactive/test/v1")
   public ResponseEntity<String> directSpringReactivetestV1() {
@@ -28,27 +26,11 @@ public class BridgeController {
         "This is a Spring Servlet microservice calling Reactive: " + reactiveResponse);
   }
 
-  @GetMapping("/gw/spring-reactive/test/v1")
-  public ResponseEntity<String> gwSpringReactiveTestV1() {
-    log.debug("Ricevuta richiesta test su endpoint bridge su reactive.");
-    String reactiveResponse = gwSpringCloudClient.callSpringReactiveGetTestV1();
-    return ResponseEntity.ok(
-        "This is a Spring Servlet microservice calling Reactive (gw): " + reactiveResponse);
-  }
-
   @GetMapping("/rust-axum/test/v1")
   public ResponseEntity<String> directRustAxumtestV1() {
     log.debug("Ricevuta richiesta test su endpoint bridge su reactive.");
     String reactiveResponse = msRustAxumClient.getTestV1();
     return ResponseEntity.ok(
         "This is a Spring Servlet microservice calling Reactive: " + reactiveResponse);
-  }
-
-  @GetMapping("/gw/rust-axum/test/v1")
-  public ResponseEntity<String> gwRustAxumTestV1() {
-    log.debug("Ricevuta richiesta test su endpoint bridge su reactive.");
-    String reactiveResponse = gwSpringCloudClient.callRustAxumGetTestV1();
-    return ResponseEntity.ok(
-        "This is a Spring Servlet microservice calling Rust (gw): " + reactiveResponse);
   }
 }
