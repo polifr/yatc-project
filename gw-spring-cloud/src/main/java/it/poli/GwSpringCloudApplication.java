b44b295586b77c15e30464af181e3524b4020063
polifr <polifr@gmail.com>
2025-08-05 01:20:15 +0200

    Fix filtri

--- gw-spring-cloud/src/main/java/it/poli/GwSpringCloudApplication.java
+++ gw-spring-cloud/src/main/java/it/poli/GwSpringCloudApplication.java
@@ -2,11 +2,13 @@ package it.poli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class GwSpringCloudApplication {
 
   public static void main(String[] args) {
+    Hooks.enableAutomaticContextPropagation();
     SpringApplication.run(GwSpringCloudApplication.class, args);
   }
 }
