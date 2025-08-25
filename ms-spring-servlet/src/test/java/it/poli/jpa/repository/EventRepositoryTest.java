f643d58ec17f33b70f59cde4df34297187440067
polifr <polifr@gmail.com>
2025-08-25 17:46:40 +0200

    Test repository

---
+++ ms-spring-servlet/src/test/java/it/poli/jpa/repository/EventRepositoryTest.java
@@ -0,0 +1,39 @@
+package it.poli.jpa.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import it.poli.jpa.model.Event;
+
+@DataJpaTest
+class EventRepositoryTest {
+
+  @Autowired
+  EventRepository eventRepository;
+
+  @Test
+  void testInjiection() {
+    assertNotNull(eventRepository, "eventRepository null");
+  }
+
+  @Test
+  void testFindAllEmpty() {
+    List<Event> items = eventRepository.findAll();
+    assertNotNull(items, "items null");
+    assertTrue(items.isEmpty(), "items not empty");
+  }
+
+  @Test
+  void testSaveAndFindAllOne() {
+    Event item = eventRepository.save(Event.builder().message("TEST MESSAGE").build());
+    assertNotNull(item.getId(), "id null");
+    List<Event> items = eventRepository.findAll();
+    assertNotNull(items, "items null");
+    assertFalse(items.isEmpty(), "items empty");
+    assertEquals(1, items.size(), "wrong items size");
+    assertEquals("TEST MESSAGE", items.getFirst().getMessage(), "wrong item message");
+  }
+
+}
