5715437fde76043236cf624545110b4105e64591
Francesco Poli <polifr@gmail.com>
2025-08-29 10:57:21 +0200

    Aggiunta nuove dashboards

--- yatc-grafana/k8s/05-create-dashboards-configmap.sh
+++ yatc-grafana/k8s/05-create-dashboards-configmap.sh
@@ -6,12 +6,20 @@ kubectl create configmap yatc-grafana-dashboard-9628 --from-file ../dashboards/9
 # Spring Cloud Gateway
 kubectl create configmap yatc-grafana-dashboard-11506 --from-file ../dashboards/11506_rev1.json
 
+# KeyCloak
+kubectl create configmap yatc-grafana-dashboard-14390 --from-file ../dashboards/14390_rev7.json
+
+# Nginx
+kubectl create configmap yatc-grafana-dashboard-14900 --from-file ../dashboards/14900_rev2.json
+
+# MongoDB
+kubectl create configmap yatc-grafana-dashboard-14997 --from-file ../dashboards/14997_rev3.json
+
+# Kafka
+kubectl create configmap yatc-grafana-dashboard-18276 --from-file ../dashboards/18276_rev1.json
+
 # Spring Boot
 kubectl create configmap yatc-grafana-dashboard-19004 --from-file ../dashboards/19004_rev1.json
 
-# TODO Altre dashboard da aggiungere:
-# nginx per fe-* https://grafana.com/grafana/dashboards/14900-nginx/
-# keycloak https://grafana.com/grafana/dashboards/14390-keycloak-metrics/
-# mongo https://grafana.com/grafana/dashboards/14997-mongodb/
-# kafka https://grafana.com/grafana/dashboards/18276-kafka-dashboard/
-# nodejs https://grafana.com/grafana/dashboards/11956-nodejs-metrics/ https://grafana.com/grafana/dashboards/19062-nodejs-applications-dashboard/ https://grafana.com/grafana/dashboards/23019-ryzen/
+# NodeJS
+kubectl create configmap yatc-grafana-dashboard-23019 --from-file ../dashboards/23019_rev1.json
