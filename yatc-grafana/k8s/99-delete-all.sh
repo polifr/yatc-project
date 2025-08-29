c9222b797c07a67f17f3f4e4d3932479940c9d3c
polifr <polifr@gmail.com>
2025-08-29 22:46:51 +0200

    Aggiunta cancellazione configmap delle dashboards

--- yatc-grafana/k8s/99-delete-all.sh
+++ yatc-grafana/k8s/99-delete-all.sh
@@ -5,6 +5,11 @@ kubectl delete deployment yatc-grafana
 
 kubectl delete configmap yatc-grafana-dashboard-9628
 kubectl delete configmap yatc-grafana-dashboard-11506
+kubectl delete configmap yatc-grafana-dashboard-14390
+kubectl delete configmap yatc-grafana-dashboard-14900
+kubectl delete configmap yatc-grafana-dashboard-14997
+kubectl delete configmap yatc-grafana-dashboard-18276
 kubectl delete configmap yatc-grafana-dashboard-19004
+kubectl delete configmap yatc-grafana-dashboard-23019
 
 kubectl delete configmap yatc-grafana-config
