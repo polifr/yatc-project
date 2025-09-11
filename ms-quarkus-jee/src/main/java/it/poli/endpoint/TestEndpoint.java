e059c22506d1b37a182bfbf39441286e7cac4bda
Francesco Poli <polifr@gmail.com>
2025-09-11 11:45:36 +0200

    Test endpoint per quarkus jee

---
+++ ms-quarkus-jee/src/main/java/it/poli/endpoint/TestEndpoint.java
@@ -0,0 +1,17 @@
+package it.poli.controller;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/quarkus-jee/test")
+public class TestEndpoint {
+
+    @Path("v1")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testV1() {
+        return "This is a Quarkus JEE microservice!";
+    }
+}
