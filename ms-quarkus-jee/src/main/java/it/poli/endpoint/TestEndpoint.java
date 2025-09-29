cdc76a4a9e90eaf6d7b4285e65f4b44c95c8ce7c
polifr <polifr@gmail.com>
2025-09-29 17:59:15 +0200

    Formattazione

--- ms-quarkus-jee/src/main/java/it/poli/endpoint/TestEndpoint.java
+++ ms-quarkus-jee/src/main/java/it/poli/endpoint/TestEndpoint.java
@@ -8,10 +8,10 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/api/quarkus-jee/test")
 public class TestEndpoint {
 
-    @Path("v1")
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String testV1() {
-        return "This is a Quarkus JEE microservice!";
-    }
+  @Path("v1")
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String testV1() {
+    return "This is a Quarkus JEE microservice!";
+  }
 }
