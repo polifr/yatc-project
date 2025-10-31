4c90e81a0c2f9d06a704b8bb401dd5abb9902e70
polifr <polifr@gmail.com>
2025-11-01 00:31:55 +0100

    Attivazione metriche prometheus

--- ms-python-fastapi/app/main.py
+++ ms-python-fastapi/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 import asyncio
 
 app = FastAPI(title="Microservice Resource Server")
@@ -6,6 +8,10 @@ app = FastAPI(title="Microservice Resource Server")
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+@app.get("/metrics")
+async def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 @app.get("/api/python-fastapi/test/v1")
 async def test():
